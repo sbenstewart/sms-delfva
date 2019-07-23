3b10556cd8681f470c8b977cd730248da0789ee7
sbenstewart <sbenstewart@gmail.com>
2019-07-23 13:08:24 +0530

    please run

--- reciever/receiver.py
+++ reciever/receiver.py
@@ -5,7 +5,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-
+@app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
     # Start our response
